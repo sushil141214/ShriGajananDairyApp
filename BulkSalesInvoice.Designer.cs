689e362e755e477092302755a50dd7afb2a3b805
Admin <lokeshyadavi14t@gmail.com>
2025-05-11 16:21:24 +0530

    Bulk Sales Invoice changes

--- BulkSalesInvoice.Designer.cs
+++ BulkSalesInvoice.Designer.cs
@@ -28,12 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "BulkSalesInvoice";
+            lblCustomer = new Label();
+            cbxCustomer = new ComboBox();
+            btnCustomerSearch = new Button();
+            dataGridView1 = new DataGridView();
+            btnCreateBulkSaleInvoice = new Button();
+            btnClear = new Button();
+            lblSelectedCount = new Label();
+            lblCount = new Label();
+            SelectCustomer = new DataGridViewCheckBoxColumn();
+            Id = new DataGridViewTextBoxColumn();
+            Customer = new DataGridViewTextBoxColumn();
+            ButtonRemoveColumn = new DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // lblCustomer
+            // 
+            lblCustomer.AutoSize = true;
+            lblCustomer.Location = new Point(25, 33);
+            lblCustomer.Name = "lblCustomer";
+            lblCustomer.Size = new Size(59, 15);
+            lblCustomer.TabIndex = 0;
+            lblCustomer.Text = "Customer";
+            // 
+            // cbxCustomer
+            // 
+            cbxCustomer.FormattingEnabled = true;
+            cbxCustomer.Location = new Point(111, 30);
+            cbxCustomer.Name = "cbxCustomer";
+            cbxCustomer.Size = new Size(481, 23);
+            cbxCustomer.TabIndex = 1;
+            // 
+            // btnCustomerSearch
+            // 
+            btnCustomerSearch.Location = new Point(629, 30);
+            btnCustomerSearch.Name = "btnCustomerSearch";
+            btnCustomerSearch.Size = new Size(75, 23);
+            btnCustomerSearch.TabIndex = 2;
+            btnCustomerSearch.Text = "Search";
+            btnCustomerSearch.UseVisualStyleBackColor = true;
+            btnCustomerSearch.Click += btnCustomerSearch_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { SelectCustomer, Id, Customer, ButtonRemoveColumn });
+            dataGridView1.Location = new Point(25, 79);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(738, 311);
+            dataGridView1.TabIndex = 3;
+            // 
+            // btnCreateBulkSaleInvoice
+            // 
+            btnCreateBulkSaleInvoice.Location = new Point(615, 415);
+            btnCreateBulkSaleInvoice.Name = "btnCreateBulkSaleInvoice";
+            btnCreateBulkSaleInvoice.Size = new Size(148, 23);
+            btnCreateBulkSaleInvoice.TabIndex = 4;
+            btnCreateBulkSaleInvoice.Text = "Create Bulk Sale Invoice";
+            btnCreateBulkSaleInvoice.UseVisualStyleBackColor = true;
+            btnCreateBulkSaleInvoice.Click += btnCreateBulkSaleInvoice_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(713, 29);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(75, 23);
+            btnClear.TabIndex = 5;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // lblSelectedCount
+            // 
+            lblSelectedCount.AutoSize = true;
+            lblSelectedCount.Location = new Point(32, 409);
+            lblSelectedCount.Name = "lblSelectedCount";
+            lblSelectedCount.Size = new Size(90, 15);
+            lblSelectedCount.TabIndex = 6;
+            lblSelectedCount.Text = "Selected Count:";
+            // 
+            // lblCount
+            // 
+            lblCount.AutoSize = true;
+            lblCount.Location = new Point(129, 409);
+            lblCount.Name = "lblCount";
+            lblCount.Size = new Size(0, 15);
+            lblCount.TabIndex = 7;
+            // 
+            // SelectCustomer
+            // 
+            SelectCustomer.HeaderText = "Select";
+            SelectCustomer.Name = "SelectCustomer";
+            SelectCustomer.Width = 50;
+            // 
+            // Id
+            // 
+            Id.HeaderText = "Id";
+            Id.Name = "Id";
+            Id.Resizable = DataGridViewTriState.True;
+            Id.SortMode = DataGridViewColumnSortMode.NotSortable;
+            Id.Visible = false;
+            // 
+            // Customer
+            // 
+            Customer.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Customer.HeaderText = "Customer";
+            Customer.Name = "Customer";
+            // 
+            // ButtonRemoveColumn
+            // 
+            ButtonRemoveColumn.HeaderText = "Remove";
+            ButtonRemoveColumn.Name = "ButtonRemoveColumn";
+            ButtonRemoveColumn.Resizable = DataGridViewTriState.True;
+            ButtonRemoveColumn.SortMode = DataGridViewColumnSortMode.Automatic;
+            ButtonRemoveColumn.Text = "Remove";
+            ButtonRemoveColumn.UseColumnTextForButtonValue = true;
+            // 
+            // BulkSalesInvoice
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblCount);
+            Controls.Add(lblSelectedCount);
+            Controls.Add(btnClear);
+            Controls.Add(btnCreateBulkSaleInvoice);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnCustomerSearch);
+            Controls.Add(cbxCustomer);
+            Controls.Add(lblCustomer);
+            Name = "BulkSalesInvoice";
+            Text = "BulkSalesInvoice";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblCustomer;
+        private ComboBox cbxCustomer;
+        private Button btnCustomerSearch;
+        private DataGridView dataGridView1;
+        private Button btnCreateBulkSaleInvoice;
+        private Button btnClear;
+        private Label lblSelectedCount;
+        private Label lblCount;
+        private DataGridViewCheckBoxColumn SelectCustomer;
+        private DataGridViewTextBoxColumn Id;
+        private DataGridViewTextBoxColumn Customer;
+        private DataGridViewButtonColumn ButtonRemoveColumn;
     }
 }
--- BulkSalesInvoice.cs
+++ BulkSalesInvoice.cs
@@ -1,20 +1,242 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ShriGajananDairy
 {
     public partial class BulkSalesInvoice : Form
     {
+        string Con = ConfigurationManager.ConnectionStrings["ShriGajananDairy.Properties.Settings.GajananDairy1"].ConnectionString;
+
         public BulkSalesInvoice()
         {
             InitializeComponent();
+
+            LoadComboBoxItems();            
+            // Enable autocomplete (search) in ComboBox
+            cbxCustomer.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            cbxCustomer.AutoCompleteSource = AutoCompleteSource.ListItems;
+            dataGridView1.CurrentCellDirtyStateChanged += dataGridView1_CurrentCellDirtyStateChanged;
+            dataGridView1.CellValueChanged += dataGridView1_CellValueChanged;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
         }
+
+        private void LoadComboBoxItems()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(Con))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("sgdGetCustomerMasterForBulk", con);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+
+                    da.Fill(dt);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        // Add items to the dropdown
+                        cbxCustomer.DataSource = dt;
+                        cbxCustomer.DisplayMember = "CustomerName";
+                        cbxCustomer.ValueMember = "Id";
+                        cbxCustomer.SelectedIndex = -1;
+                        //cbxCustomer.SelectedText = "Select Customer";                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Try again later");
+                return;
+            }
+        }
+
+        private void ClearDetails()
+        {
+            cbxCustomer.SelectedIndex = -1;
+            dataGridView1.Rows.Clear();
+        }
+        
+        private void dataGridView1_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.IsCurrentCellDirty)
+            {
+                dataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit);
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["SelectCustomer"].Index) // Adjust "Select" to your checkbox column name
+            {
+                int count = 0;
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (Convert.ToBoolean(row.Cells["SelectCustomer"].Value) == true)
+                    {
+                        count++;
+                    }
+                }
+
+                lblCount.Text = count.ToString(); // You can update a label, or use MessageBox
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {            
+            if (e.RowIndex >= 0 && dataGridView1.Columns[e.ColumnIndex].Name == "ButtonRemoveColumn")
+            {
+                var confirm = MessageBox.Show("Do you want to remove this row?", "Confirm Delete", MessageBoxButtons.YesNo);
+                if (confirm == DialogResult.Yes)
+                {
+                    dataGridView1.Rows.RemoveAt(e.RowIndex);
+                    cbxCustomer.SelectedIndex = -1;
+                }
+            }
+        }
+
+        private void btnCustomerSearch_Click(object sender, EventArgs e)
+        {
+            if (cbxCustomer.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a customer from the dropdown.");
+                return;
+            }
+
+            string selected = cbxCustomer.Text;
+
+            if (selected == "Select All")
+            {
+                using (SqlConnection con = new SqlConnection(Con))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("sp_GetCustomerMaster", con);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dtg = new DataTable();
+
+                    da.Fill(dtg);
+
+                    if (dtg.Rows.Count > 0)
+                    {
+                        for (int i = 0; i < dtg.Rows.Count; i++)
+                        {
+                            AddCustomerToGrid(Convert.ToInt64(dtg.Rows[i][0]), dtg.Rows[i][1].ToString());
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (dataGridView1.Rows.Count == 0)
+                {
+                    AddCustomerToGrid(Convert.ToInt64(cbxCustomer.SelectedValue), cbxCustomer.Text);                
+                }
+                else
+                {
+                    AddCustomerToGrid(Convert.ToInt64(cbxCustomer.SelectedValue), cbxCustomer.Text);
+                }
+            }
+        }
+
+        private void AddCustomerToGrid(Int64 customerId,string customerName)
+        {
+            // Prevent duplicate entries
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells["Customer"].Value?.ToString() == customerName)
+                    return;
+            }
+                        
+            dataGridView1.Rows.Add(false, customerId, customerName);
+        }
+
+        private void btnCreateBulkSaleInvoice_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool anyChecked = dataGridView1.Rows
+                                  .Cast<DataGridViewRow>()
+                                  .Any(row => Convert.ToBoolean(row.Cells["SelectCustomer"].Value) == true);
+
+                if (!anyChecked)
+                {
+                    MessageBox.Show("Please select at least one record.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return; // Exit the method early
+                }
+
+                List<string> duplicates = new List<string>();
+                int insertCount = 0;
+
+                using (SqlConnection conn = new SqlConnection(Con))
+                {
+                    conn.Open();
+
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        if (Convert.ToBoolean(row.Cells["SelectCustomer"].Value) == true)
+                        {
+                            string customerName = row.Cells["Customer"].Value.ToString();
+                            Int64 customerId = Convert.ToInt64(row.Cells["Id"].Value);
+
+                            SqlCommand cmd = new SqlCommand("sgd_InsertCustomerIfNotExists", conn);
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            cmd.Parameters.AddWithValue("@CustomerId", customerId);
+                            cmd.Parameters.AddWithValue("@CustomerName", customerName);
+                            cmd.Parameters.AddWithValue("@CreatedBy", "Admin");
+
+                            // Get return value from stored procedure
+                            SqlParameter returnValue = new SqlParameter();
+                            returnValue.Direction = ParameterDirection.ReturnValue;
+                            cmd.Parameters.Add(returnValue);
+
+                            cmd.ExecuteNonQuery();
+
+                            int result = (int)returnValue.Value;
+                            if (result == 1)
+                            {
+                                insertCount++; // Inserted
+                            }
+                            else
+                            {
+                                duplicates.Add(customerName); // Duplicate
+                            }
+                        }
+                    }
+                }
+
+                // Show result
+                string message = $" Inserted: {insertCount} record(s).";
+                if (duplicates.Count > 0)
+                {
+                    message += "\n Skipped (already exists):\n" + string.Join("\n", duplicates);
+                }
+
+                MessageBox.Show(message, "Insert Summary");
+                ClearDetails();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Try again later");
+                return;
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearDetails();
+        }
+
     }
 }
